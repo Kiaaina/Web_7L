4e3447ae9948ccf12f9b0edfffa761b30100db1f
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2025-06-18 10:47:53 -1000

    Add files via upload

--- 7L.cmd
+++ 7L.cmd
@@ -1,8 +1,8 @@
 cd /
 title HTML
 @echo off
-mode 100,5
-color 71
+mode 70,5
+color 04
 cls
-start http://htmlpreview.github.io/?https://github.com/Kiaaina/Web_7L/blob/main/7L.html
+"C:\Program Files\Google\Chrome\Application\chrome.exe" C:\Users\Persiden\Desktop\Web_7L-main\7L.html
 exit
