b87008da87de9f3e225fc68a69482ccdb4ea0409
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2025-06-18 16:28:47 -1000

    Add files via upload

--- 7L.cmd
+++ 7L.cmd
@@ -4,5 +4,5 @@ title HTML
 mode 70,5
 color 04
 cls
-"C:\Program Files\Google\Chrome\Application\chrome.exe" C:\Users\Persiden\Desktop\Web_7L-main\7L.html
+"C:\Program Files\Google\Chrome\Application\chrome.exe" %UserProfile%\Desktop\Web_7L-main\7L.html
 exit
